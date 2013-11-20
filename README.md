374f8d95980365bd4e1dba54a7c2944292718872
Brian Johnson <bjohnson@adsummos.com>
2013-11-20 15:01:03 -0500

    Make things more serious

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ seriousify
 ==========
 
 Serious business for your synergistic customer-centric applications
+
+- scale interactive e-tailers
+
+- aggregate enterprise technologies
+
+- integrate efficient infrastructures
+
+- orchestrate intuitive technologies
+
+usage:
+
+class Business
+  include Seriousify
+
+  def revenue
+    sleep 1000
+    Rand(1000)
+  end
+end
+
+Business.new.revenue => "redefine global supply-chains"
+Business.new.revenue_seriously => 47
---
+++ lib/seriousify.rb
@@ -0,0 +1,40 @@
+module Seriousify
+  def self.included(base)
+    base.instance_eval do
+      instance_methods(false).each do |method|
+        alias_method "#{method}_seriously", method
+        define_method(method) { generate_bs }
+      end
+    end
+  end
+
+  def generate_bs
+
+    array1 = ["implement", "utilize", "integrate", "streamline", "optimize", "evolve", "transform", "embrace", 
+    "enable", "orchestrate", "leverage", "reinvent", "aggregate", "architect", "enhance", "incentivize", "morph", "empower", 
+    "envisioneer", "monetize", "harness", "facilitate", "seize", "disintermediate", "synergize", "strategize", "deploy", 
+    "brand", "grow", "target", "syndicate", "synthesize", "deliver", "mesh", "incubate", "engage", "maximize", "benchmark", 
+    "expedite", "reintermediate", "whiteboard", "visualize", "repurpose", "innovate", "scale", "unleash", "drive", "extend", 
+    "engineer", "revolutionize", "generate", "exploit", "transition", "e-enable", "iterate", "cultivate", "matrix", 
+    "productize", "redefine", 
+    "recontextualize"]
+
+    array2 = ["clicks-and-mortar", "value-added", "vertical", "proactive", "robust", "revolutionary", "scalable", 
+    "leading-edge", "innovative", "intuitive", "strategic", "e-business", "mission-critical", "sticky", "one-to-one", 
+    "24/7", "end-to-end", "global", "B2B", "B2C", "granular", "frictionless", "virtual", "viral", "dynamic", "24/365", 
+    "best-of-breed", "killer", "magnetic", "bleeding-edge", "web-enabled", "interactive", "dot-com", "sexy", "back-end", 
+    "real-time", "efficient", "front-end", "distributed", "seamless", "extensible", "turn-key", "world-class", 
+    "open-source", "cross-platform", "cross-media", "synergistic", "bricks-and-clicks", "out-of-the-box", "enterprise", 
+    "integrated", "impactful", "wireless", "transparent", "next-generation", "cutting-edge", "user-centric", "visionary", 
+    "customized", "ubiquitous", "plug-and-play", "collaborative", "compelling", "holistic", "rich"]
+
+    array3 = ["synergies", "web-readiness", "paradigms", "markets", "partnerships", "infrastructures", "platforms", 
+    "initiatives", "channels", "eyeballs", "communities", "ROI", "solutions", "e-tailers", "e-services", "action-items", 
+    "portals", "niches", "technologies", "content", "vortals", "supply-chains", "convergence", "relationships", 
+    "architectures", "interfaces", "e-markets", "e-commerce", "systems", "bandwidth", "infomediaries", "models", 
+    "mindshare", "deliverables", "users", "schemas", "networks", "applications", "metrics", "e-business", "functionalities", 
+    "experiences", "web services", "methodologies"]
+
+    "#{array1[Random.rand(array1.size-1)]} #{array2[Random.rand(array2.size-1)]} #{array3[Random.rand(array3.size-1)]}"
+  end
+end
